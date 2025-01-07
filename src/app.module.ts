593cfd59b1f30bf8cb171a464758fcc2ecf6e9e3
shilianmalaxiangguo <695614960@qq.com>
2025-01-07 23:50:53 +0800

    feat: 添加注释

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,26 @@
-import type { MiddlewareConsumer, NestModule } from '@nestjs/common'
-import { Module } from '@nestjs/common'
-import { HttpModule } from '@nestjs/axios'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { UsersModule } from './users/users.module'
-import { PrismaService } from './prisma/prisma.service'
-import { LoggerMiddleware } from './middleware/logger.middleware'
-import { ProxyMiddleware } from './middleware/proxy.middleware'
+// src/app.module.ts
+import type { MiddlewareConsumer, NestModule } from '@nestjs/common' // 导入 NestJS 的 MiddlewareConsumer 和 NestModule 类型
+import { Module } from '@nestjs/common' // 导入 NestJS 的 Module 装饰器
+import { HttpModule } from '@nestjs/axios' // 导入 HttpModule，用于处理 HTTP 请求
+import { AppController } from './app.controller' // 导入应用控制器
+import { AppService } from './app.service' // 导入应用服务
+import { UsersModule } from './users/users.module' // 导入用户模块
+import { PrismaService } from './prisma/prisma.service' // 导入 Prisma 服务，用于数据库交互
+import { LoggerMiddleware } from './middleware/logger.middleware' // 导入日志中间件
+import { ProxyMiddleware } from './middleware/proxy.middleware' // 导入代理中间件
 
 @Module({
   imports: [
-    HttpModule,
-    UsersModule
+    HttpModule, // 注册 HttpModule
+    UsersModule // 注册 UsersModule
   ],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [AppController], // 注册应用控制器
+  providers: [AppService, PrismaService], // 注册应用服务和 Prisma 服务
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
+export class AppModule implements NestModule { // 定义 AppModule 类并实现 NestModule 接口
+  configure(consumer: MiddlewareConsumer) { // 配置中间件
     consumer
-      .apply(ProxyMiddleware, LoggerMiddleware)
-      .forRoutes('*')
+      .apply(ProxyMiddleware, LoggerMiddleware) // 应用代理和日志中间件
+      .forRoutes('*') // 为所有路由应用中间件
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common'
-import { UsersController } from './controllers/users.controller'
-import { UsersService } from './services/users.service'
-import { PrismaService } from '../prisma/prisma.service'
+// src/users/users.module.ts
+import { Module } from '@nestjs/common' // 导入 NestJS 的 Module 装饰器
+import { UsersController } from './controllers/users.controller' // 导入用户控制器
+import { UsersService } from './services/users.service' // 导入用户服务
+import { PrismaService } from '../prisma/prisma.service' // 导入 Prisma 服务，用于数据库交互
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  controllers: [UsersController], // 注册用户控制器
+  providers: [UsersService, PrismaService], // 注册用户服务和 Prisma 服务
 })
-export class UsersModule {} 
+export class UsersModule {} // 导出 UsersModule 类
